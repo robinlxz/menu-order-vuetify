ce28035bcc112d760cb94d8c1c78a6e569493095
Xuzheng <linxz@garena.com>
2020-06-04 09:24:40 +0800

    'ADD: clear homepage, add todo list for 0604'

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,33 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <!-- <img alt="Vue logo" src="../assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js App" />-->
+    <h1>This is SingHey homepage</h1>
+    <p>Todo list</p>
+    <ul class="todolist">
+      <li>Menu, not really need 'EventShow' view</li>
+      <li>Form to submit order, save in local json as fake db</li>
+      <li>Put above system to Google Cloud</li>
+      <li>To enable SASS in this project</li>
+      <li>*CSS adjustment, materialize maybe</li>
+    </ul>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
+// import HelloWorld from "@/components/HelloWorld.vue";
 
 export default {
   name: "Home",
   components: {
-    HelloWorld
+    // HelloWorld
   }
 };
 </script>
+
+<style scoped>
+.todolist {
+  list-style: none;
+}
+</style>>
