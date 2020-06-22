0a1ebc90fc37a9cb25bf29ecbc06eec648c21643
Xuzheng <linxz@garena.com>
2020-06-22 19:08:19 +0800

    ADD: Pick date time, and special reqeust text input

--- src/components/DeliveryInfoForm.vue
+++ src/components/DeliveryInfoForm.vue
@@ -2,6 +2,45 @@
   <div>
     <v-form>
       <v-container>
+        <v-row justify="center">
+          <v-col cols="6" sm="6" md="4">
+            <v-menu
+              :close-on-content-click="true"
+              :nudge-right="40"
+              transition="scale-transition"
+              offset-y
+              min-width="290px"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field
+                  v-model="date"
+                  label="Date for pickup/delivery"
+                  readonly
+                  v-bind="attrs"
+                  v-on="on"
+                ></v-text-field>
+              </template>
+              <v-date-picker v-model="date" @input="menu2 = false"></v-date-picker>
+            </v-menu>
+          </v-col>
+          <v-col cols="6" sm="6">
+            <v-menu
+              ref="menu"
+              :close-on-content-click="false"
+              :nudge-right="40"
+              :return-value.sync="time"
+              transition="scale-transition"
+              offset-y
+              max-width="290px"
+              min-width="290px"
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-text-field v-model="time" label="Time" readonly v-bind="attrs" v-on="on"></v-text-field>
+              </template>
+              <v-time-picker v-model="time" full-width @click:minute="$refs.menu.save(time)"></v-time-picker>
+            </v-menu>
+          </v-col>
+        </v-row>
         <v-row>
           <v-col cols="12" md="4">
             <v-text-field v-model="name" :counter="20" label="Name" required></v-text-field>
@@ -9,6 +48,10 @@
 
           <v-col cols="12" md="4">
             <v-text-field v-model="phone" :counter="8" label="Phone No." required></v-text-field>
+          </v-col>
+
+          <v-col cols="12" md="4">
+            <v-text-field v-model="reqeustInfo" :counter="100" label="Special Request"></v-text-field>
           </v-col>
 
           <v-col cols="12" md="4" v-if="errors.length">
@@ -44,7 +87,11 @@ export default {
       name: '',
       errors: [],
       successInfo: '',
-      submitErrorAlert: ''
+      submitErrorAlert: '',
+      // date: new Date().toISOString().substr(0, 10),
+      date: null,
+      time: null,
+      reqeustInfo: ''
     }
   },
   methods: {
@@ -60,6 +107,9 @@ export default {
       this.$store.state.cart = []
       this.name = ''
       this.phone = ''
+      this.date = null
+      this.time = null
+      this.reqeustInfo = ''
     },
     sendEmail() {
       this.errors = []
@@ -67,6 +117,8 @@ export default {
       // Validate order
       if (this.$store.state.cart.length == 0) {
         this.errors.push('Seemed your order is empty #_#')
+      } else if (this.date == null || this.time == null) {
+        this.errors.push('Please pick date and time')
       } else if (this.name == '' || this.phone.length !== 8) {
         if (this.name == '') this.errors.push('Please fill in your name')
         if (this.phone.length !== 8)
@@ -74,8 +126,10 @@ export default {
       } else {
         const templateParams = {
           name: [this.name, this.phone],
-          number: this.phone,
-          dishes: this.$store.state.cart.map(d => d.title)
+          number: this.phone, //emailjs fail to show this in template, idk why
+          dishes: this.$store.state.cart.map(d => d.title),
+          timestamp: [this.date, this.time],
+          requestInfo: [this.reqeustInfo]
         }
 
         // console.log(templateParams)
@@ -101,6 +155,9 @@ export default {
             }
           )
       }
+    },
+    notsendEmail() {
+      console.log(this.date, this.time)
     }
   }
 }
